38eccd5979cfcf7a102d9a2cf25c8c5dc00f95ef
RexT2507 <baptiste.marmier@ynov.com>
2019-11-15 11:06:57 +0100

    [ADD] [DEV] Ajout du model User et mise en place des différentes méthodesd de l'API

---
+++ server/models/User.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    name: String,
+    surname: String,
+    email: String,
+    password: String,
+    account: Number,
+    bank: String,
+});
+
+module.exports = mongoose.model('user', userSchema, 'users');
